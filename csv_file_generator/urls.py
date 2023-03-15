5a30ebf357a0034af2bc7dec94b955166c28d061
Kostyantyn Chernyk <chernykko@gmail.com>
2023-03-15 17:55:39 +0000

    Connect static files to project

--- csv_file_generator/urls.py
+++ csv_file_generator/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,4 +7,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("schema.urls", namespace="schema")),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
