9c821ebfa187e47d9287471613e22159e1179d6c
Kostyantyn Chernyk <chernykko@gmail.com>
2023-03-15 15:23:25 +0000

    Add simple schema creation view, endpoint and template

--- schema/urls.py
+++ schema/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from schema.views import SchemaListView
+from schema.views import SchemaListView, create_schema
 
 urlpatterns = [
     path("", SchemaListView.as_view(), name="schema-list"),
+    path("schema/create/", create_schema, name="schema-create"),
 ]
 
 app_name = "schema"
--- schema/views.py
+++ schema/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import generic
 
+from schema.forms import SchemaCreationForm
 from schema.models import Schema
 
 
@@ -11,3 +14,17 @@ class SchemaListView(
 ):
     model = Schema
     queryset = Schema.objects.all()
+
+
+def create_schema(request):
+    schema_form = SchemaCreationForm(request.POST or None, user=request.user)
+
+    context = {
+        "schema_form": schema_form
+    }
+
+    if schema_form.is_valid():
+        schema_form.save()
+        return HttpResponseRedirect(reverse("schema:schema-list"))
+
+    return render(request, "schema/schema_form.html", context=context)
